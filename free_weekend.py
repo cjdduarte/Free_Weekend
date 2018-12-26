e30246fa8e206dd7745583c0333f5d44355c7751
Carlos Duarte <cjdduarte@gmail.com>
2018-12-26 21:59:11 -0200

    add parameters so that specific groups of decks are not affected by this addon

--- free_weekend.py
+++ free_weekend.py
@@ -9,26 +9,36 @@ import sys
 import anki
 import datetime
 from aqt import mw
-from anki import version
-from anki.sched import Scheduler
 
+from anki import version
+ANKI21 = version.startswith("2.1.")
+
+from anki.sched import Scheduler
 from aqt.utils import tooltip
-#import os
+
+import aqt
+import aqt.deckconf
+from anki.hooks import wrap
+
+if ANKI21:
+    from PyQt5 import QtCore, QtGui, QtWidgets
+else:
+    from PyQt4 import QtCore, QtGui as QtWidgets
+
 from random import *
 
 #-------------Configuration------------------
+'''
 try:
     config = mw.addonManager.getConfig(__name__)
 except AttributeError:
     config = dict(days_week=[6], log_tooltip=0, specific_days=["9999/12/31"])
 '''
-if getattr(mw.addonManager, "getConfig", None): #Anki 2.1
+if getattr(getattr(mw, "addonManager", None), "getConfig", None):   #Anki 2.1
     config = mw.addonManager.getConfig(__name__)
-    tooltip("Anki 2.1", period=3000)
-else:  #Anki 2.0
+else:                                                               #Anki 2.0
     config = dict(days_week=[6], log_tooltip=0, specific_days=["9999/12/31"])
-    tooltip("Anki 2.0", period=3000)
-'''
+
 #-------------Configuration (Anki 2.0) ------------------
 #days_week      = [6]      #0=Mon, 1=Tue, 2=Wed, 3=Thu, 4=Fri, 5=Sat, 6=Sun, -1=Ignore
 #log_tooltip    = 0        #"0=OFF, 1=Basic, 2=More"
@@ -46,9 +56,65 @@ user_files_dir = os.path.join(this_script_dir, 'datas.txt')
 with open(user_files_dir, 'r', encoding='utf-8') as f:
     datas_arquivo = [line.strip() for line in f]'''
 
-def load_balanced_ivl(self, ivl):
+'''def dconfsetupUi(self, Dialog):
+    r=0
+    tabDF = QtWidgets.QWidget()
+    vLayoutDF = QtWidgets.QVBoxLayout(tabDF)
+    layoutDF = QtWidgets.QGridLayout()
+
+    self.defuzz = QtWidgets.QCheckBox(self.tab_3)
+    self.defuzz.setText(_('Disable Free Weekend'))
+    layoutDF.addWidget(self.defuzz, r, 0, 1, 1)
+    r+=1
+
+    lb_defuzz_ivl2 = QtWidgets.QLabel(tabDF)
+    lb_defuzz_ivl2.setText(_("ivl max @ ivl=2:"))
+    layoutDF.addWidget(lb_defuzz_ivl2, r, 0, 1, 1)
+
+    self.defuzz_ivl2 = QtWidgets.QSpinBox(tabDF)
+    self.defuzz_ivl2.setMinimum(2)
+    self.defuzz_ivl2.setMaximum(69)
+    layoutDF.addWidget(self.defuzz_ivl2, r, 1, 1, 1)
+    r+=1
+
+    lb_defuzz_ivl7 = QtWidgets.QLabel(tabDF)
+    lb_defuzz_ivl7.setText(_("%fuzz @ ivl<7: (0=disable)"))
+    layoutDF.addWidget(lb_defuzz_ivl7, r, 0, 1, 1)
+
+    self.defuzz_ivl7 = QtWidgets.QSpinBox(tabDF)
+    self.defuzz_ivl7.setMinimum(0)
+    self.defuzz_ivl7.setMaximum(666)
+    layoutDF.addWidget(self.defuzz_ivl7, r, 1, 1, 1)
+    r+=1
+
+    vLayoutDF.addLayout(layoutDF)
+    spacerItem1 = QtWidgets.QSpacerItem(20, 152, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+    vLayoutDF.addItem(spacerItem1)
+    self.tabWidget.addTab(tabDF, "Free Weekend")'''
+
+def setup_ui(self, Dialog):
+
+    grid = QtWidgets.QGridLayout()
+
+    self.DisableFW = QtWidgets.QCheckBox(self.tab_5)
+    self.DisableFW.setText(_('Disable Free Weekend (It will affect all decks that belong to this group of options)'))
+    grid.addWidget(self.DisableFW, 0, 0, 1, 1)
+
+    self.verticalLayout_6.insertLayout(1, grid)
+
+def load_conf(self):
+    f = self.form
+    c = self.conf
+    f.DisableFW.setCheckState(c.get('DisableFW', 0))
+
+def save_conf(self):
+    f = self.form
+    c = self.conf
+    c['DisableFW'] = f.DisableFW.checkState()
+
+def load_balanced_ivl(sched, ivl, _old):
     orig_ivl = int(ivl)
-    min_ivl, max_ivl = self._fuzzIvlRange(orig_ivl)
+    min_ivl, max_ivl = sched._fuzzIvlRange(orig_ivl)
     IvlRangeOri = []
     IvlRange = []
     ignored_days = []
@@ -71,7 +137,16 @@ def load_balanced_ivl(self, ivl):
 
     ignored_days = ', '.join(ignored_days)
 
-    if removed_all:
+    #--------Deck ignored by parameter--------
+    ignore_deck = False
+    card = mw.reviewer.card
+    if not card: return True
+    conf=sched.col.decks.confForDid(card.odid or card.did)
+    if conf.get('DisableFW',0) == 2:
+        ignore_deck = True
+    #--------Deck ignored by parameter--------
+
+    if removed_all or ignore_deck:
         best_ivl = choice(IvlRangeOri)
     else:
         best_ivl = choice(IvlRange)
@@ -97,6 +172,13 @@ def load_balanced_ivl(self, ivl):
         elif log_tooltip == 2:
             mensagem = log_min_ivl + log_max_ivl + log_best_ivl + 'ignored=All days used! Range Fuzz too small.'
             tooltip(mensagem, period=4000)
+    elif  ignore_deck:
+        if log_tooltip == 1:
+            mensagem = 'Disable Free Weekend in Options Group.'
+            tooltip(mensagem, period=3000)
+        elif log_tooltip == 2:
+            mensagem = log_min_ivl + log_max_ivl + log_best_ivl + 'Disable Free Weekend in Options Group.'
+            tooltip(mensagem, period=4000)
     elif log_tooltip and ignored_days:
         if log_tooltip == 1:
             mensagem = log_ign_days
@@ -108,10 +190,15 @@ def load_balanced_ivl(self, ivl):
 
     return best_ivl
 
+#aqt.forms.dconf.Ui_Dialog.setupUi = wrap(aqt.forms.dconf.Ui_Dialog.setupUi, dconfsetupUi, pos="after")
+aqt.forms.dconf.Ui_Dialog.setupUi   = wrap(aqt.forms.dconf.Ui_Dialog.setupUi, setup_ui, pos="after")
+aqt.deckconf.DeckConf.loadConf      = wrap(aqt.deckconf.DeckConf.loadConf, load_conf, pos="after")
+aqt.deckconf.DeckConf.saveConf      = wrap(aqt.deckconf.DeckConf.saveConf, save_conf, pos="before")
+
 # Patch Anki 2.0 and Anki 2.1 default scheduler
-anki.sched.Scheduler._fuzzedIvl = load_balanced_ivl
+anki.sched.Scheduler._fuzzedIvl = wrap(anki.sched.Scheduler._fuzzedIvl, load_balanced_ivl, 'around')
 
 # Patch Anki 2.1 experimental v2 scheduler
 if version.startswith("2.1"):
     from anki.schedv2 import Scheduler
-    anki.schedv2.Scheduler._fuzzedIvl = load_balanced_ivl
+    anki.schedv2.Scheduler._fuzzedIvl = wrap(anki.schedv2.Scheduler._fuzzedIvl, load_balanced_ivl, 'around')
